5cabb48047542b2e8809301b27d77343020eb96f
myoungho.pak <qkraudghgh@gmail.com>
2017-06-04 15:54:32 +0900

    Setting Calendar DemoApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarComponent } from 'angular2-fullcalendar/src/calendar/calendar';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CalendarAppComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CalendarAppComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,74 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html'
+})
+export class CalendarAppComponent {
+  calendarOptions: Object = {
+    fixedWeekCount : false,
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,basicWeek,basicDay'
+    },
+    editable: false,
+    eventLimit: true, // allow "more" link when too many events
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2017-06-01'
+      },
+      {
+        title: 'Long Event',
+        start: '2017-06-07',
+        end: '2017-06-10'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2017-06-09T16:00:00'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: '2017-06-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: '2017-06-11',
+        end: '2016-09-13'
+      },
+      {
+        title: 'Meeting',
+        start: '2017-06-12T10:30:00',
+        end: '2017-06-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: '2017-06-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: '2017-06-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: '2017-06-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: '2017-06-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: '2017-06-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2017-06-28'
+      }
+    ]
+  };
+}
